fd42f78a470860e2431c86caf60c923a58ac6160
Eeernest <ernestgrabski16@wp.pl>
2026-09-07 14:16:33 +0200

    feat(user): add User class to setup users table with SQLAlchemy

---
+++ app/models/user_model.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+
+from app.db.database import Base
+
+class User(Base):
+  __tablename__ = "users"
+
+  id = Column(Integer, primary_key=True, index=True)
+  username = Column(String, nullable=False, unique=True, index=True)
+  email = Column(String, nullable=False, unique=True, index=True)
+  hashed_password = Column(String, nullable=False)
+  user_role = Column(String, index=True)
+  is_active = Column(Boolean)
+  is_deleted = Column(Boolean)
+  created_at = Column(DateTime, default=datetime.utcnow)
+  updated_at = Column(DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
