08c43179e9c11ee7ec6949a73aeb6a03025143c2
Eeernest <ernestgrabski16@wp.pl>
2026-09-07 13:57:21 +0200

    feat(config): add Config class to manage secrets from .env file

---
+++ app/core/config.py
@@ -0,0 +1,18 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+class Config(BaseSettings):
+  # PostgreSQL
+
+  POSTGRES_URL: str
+
+
+  # model config
+
+  model_config = SettingsConfigDict(
+    env_file=".env",
+    extra="ignore",
+    case_sensitive=True
+  )
+
+
+settings = Config()
