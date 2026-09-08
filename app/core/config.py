0a8e4035cd3ee30232cabc38aa1e8cd2e841d6c9
Eeernest <ernestgrabski16@wp.pl>
2026-09-08 22:29:40 +0200

    update(condig): add admin credentials to Config class to use them for bulk insert in alembic migraion

--- app/core/config.py
+++ app/core/config.py
@@ -11,6 +11,13 @@ class Config(BaseSettings):
   ALEMBIC_DB_URL: str
 
 
+  # Admin Credentials
+
+  ADMIN_USERNAME: str = "ADMIN_USERNAME"
+  ADMIN_EMAIL: str = "ADMIN_EMAIL"
+  ADMIN_HASHED_PASSWORD: str = "ADMIN_HASHED_PASSWORD"
+
+
   # model config
 
   model_config = SettingsConfigDict(
