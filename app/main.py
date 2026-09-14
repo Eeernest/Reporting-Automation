2f63b7c94fa86c9f784b26b84140a1fa624fbb40
Eeernest <ernestgrabski16@wp.pl>
2026-09-14 15:39:31 +0200

    feat(user): add parser function to main.py to give user CLI tool that can connect with API

--- app/main.py
+++ app/main.py
@@ -1,10 +1,15 @@
-from fastapi import FastAPI
+import argparse
 
+from fastapi import FastAPI
 from fastapi.exceptions import RequestValidationError
 
+from app.cli.user_cli import setup_user_parser
+from app.core.config import settings
 from app.core.exceptions import AppError
 from app.core.exception_handler import custom_exc_handler, validation_exc_handler
 from app.routers.user_router import router as user_router
+
+# API
 
 app = FastAPI()
 
@@ -12,3 +17,34 @@ app.add_exception_handler(AppError, custom_exc_handler)
 app.add_exception_handler(RequestValidationError, validation_exc_handler)
 
 app.include_router(user_router)
+
+
+# CLI
+
+def parser():
+  parser = argparse.ArgumentParser(description="Main CLI for API")
+
+  parser.add_argument(
+    "--api-url",
+    default=settings.APP_URL,
+    help="API URL Address"
+  )
+
+  resource_subparser = parser.add_subparsers(
+    dest="resource",
+    required=True,
+    help="Resources"
+  )
+
+  setup_user_parser(resource_subparser)
+
+  args = parser.parse_args()
+
+  if hasattr(args, "func"):
+    args.func(args)
+
+  else:
+    parser.print_help()
+
+if __name__ == "__main__":
+  parser()
