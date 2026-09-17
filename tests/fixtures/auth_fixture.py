2259df444f24e3aa55857296777b815207cedd45
Eeernest <ernestgrabski16@wp.pl>
2026-09-17 19:35:14 +0200

    test(auth): add unit tests for login function from auth_router.py

--- tests/fixtures/auth_fixture.py
+++ tests/fixtures/auth_fixture.py
@@ -1,17 +1,34 @@
+from unittest.mock import AsyncMock
+
+from httpx import AsyncClient, ASGITransport
 import pytest
 
+from app.dependencies.auth_dependency import get_auth_service
+from app.main import app
 from app.services.auth_service import AuthService
 
 # Unit
 
-@pytest.fixture()
+@pytest.fixture
 def unit_auth_service(mock_security, mock_token_repo, mock_user_repo):
   return AuthService(mock_security, mock_token_repo, mock_user_repo)
 
+@pytest.fixture
+def mock_auth_service():
+  return AsyncMock()
+
+@pytest.fixture
+async def unit_auth_client(mock_auth_service):
+  app.dependency_overrides[get_auth_service] = lambda: mock_auth_service
+  
+  async with AsyncClient(transport=ASGITransport(app=app), base_url="http://test") as c:
+    yield c
+
+  app.dependency_overrides.clear()
 
 # Helper
 
-@pytest.fixture()
+@pytest.fixture
 def token_dict():
   return {
     "jti": "1",
---
+++ tests/routers/auth_router_test.py
@@ -0,0 +1,45 @@
+from fastapi import status
+import pytest
+
+import app.core.exceptions as e
+
+# login
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_login_success(
+  mock_auth_service,
+  unit_auth_client,
+  token_response_obj
+):
+  mock_auth_service.login.return_value = token_response_obj
+
+  result = await unit_auth_client.post("/login", data={
+    "username": "user1",
+    "password": "Password123"
+  })
+
+  data = result.json()
+
+  assert result.status_code == status.HTTP_200_OK
+  assert data["access_token"] == token_response_obj.access_token
+  assert data["refresh_token"] == token_response_obj.refresh_token
+  assert data["token_type"] == token_response_obj.token_type
+
+@pytest.mark.anyio
+@pytest.mark.unit
+async def test_login_invalid_credentials_error(
+  mock_auth_service,
+  unit_auth_client
+):
+  mock_auth_service.login.side_effect = e.InvalidCredentialsError()
+
+  result = await unit_auth_client.post("/login", data={
+    "username": "user1",
+    "password": "wrongpassword"
+  })
+
+  data = result.json()
+
+  assert result.status_code == e.InvalidCredentialsError.status_code
+  assert data["detail"] == e.InvalidCredentialsError.detail
