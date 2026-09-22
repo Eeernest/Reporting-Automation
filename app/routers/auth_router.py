7faf78fe9fc7e8cd4bd92fd751ce598cf94ed3ec
Eeernest <ernestgrabski16@wp.pl>
2026-09-22 22:15:42 +0200

    feat(auth): add logout function to auth_router.py file

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.dependencies.auth_dependency import AuthServiceDep
-from app.schemas.token_schema import TokenResponse
+from app.schemas.token_schema import TokenLogoutRequest, TokenResponse
 
 router = APIRouter()
 
@@ -16,3 +16,9 @@ async def login(
   login_data: OAuth2PasswordRequestForm = Depends()
 ):
   return await service.login(login_data.username, login_data.password)
+
+@router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
+async def logout(service: AuthServiceDep, logout_request: TokenLogoutRequest):
+  await service.logout(logout_request)
+
+  return status.HTTP_204_NO_CONTENT
