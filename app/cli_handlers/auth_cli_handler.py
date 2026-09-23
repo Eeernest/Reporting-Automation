27e92ec918686c505569b55756a33d6fc6bd90d9
Eeernest <ernestgrabski16@wp.pl>
2026-09-23 21:21:29 +0200

    feat(auth): add auth_logout_handler function to auth_cli_handler.py file

--- app/cli_handlers/auth_cli_handler.py
+++ app/cli_handlers/auth_cli_handler.py
@@ -6,34 +6,57 @@ import requests
 from app.core.config import settings
 
 APP_URL = settings.APP_URL
+token_file = Path.home() / settings.TOKEN_STORAGE_PATH
 
 # Main Functions
 
 def auth_login_handler(args: Namespace) -> None:
   endpoint_url = f"{APP_URL}/login"
 
-  payload = {
+  request_payload = {
     "username": args.username,
     "password": args.password
   }
 
   try:
-    response = requests.post(endpoint_url, data=payload)
+    response = requests.post(endpoint_url, data=request_payload)
     response.raise_for_status()
 
     token_data = response.json()
 
     token_payload = {
-      "access_token": token_data.get("access_token"),
-      "refresh_token": token_data.get("refresh_token"),
-      "token_type": token_data.get("token_type") 
+      "access_token": token_data["access_token"],
+      "refresh_token": token_data["refresh_token"],
+      "token_type": token_data["token_type"]
     }
 
-    _save_tokens(
-      token_payload
-    )
+    _save_tokens(token_payload)
 
     print("User successfully logged in")
+
+  except requests.exceptions.HTTPError as err:
+    print(f"HTTP Error: {err}")
+
+    if response.content:
+      print(f"Detail: {response.text}")
+
+def auth_logout_handler(args: Namespace) -> None:
+  endpoint_url = f"{APP_URL}/logout"
+
+  token_data = _load_tokens()
+
+  if not token_data or not token_data["refresh_token"]:
+    print("Refresh token error. Login again")
+    return
+
+  request_payload = {"refresh_token": token_data["refresh_token"]}
+
+  try:
+    response = requests.post(endpoint_url, json=request_payload)
+
+    token_file.unlink()
+
+    print("User successfully logged out")
 
   except requests.exceptions.HTTPError as err:
     print(f"HTTP Error: {err}")
@@ -45,9 +68,18 @@ def auth_login_handler(args: Namespace) -> None:
 # Helper Functions
 
 def _save_tokens(token_payload: dict) -> None:
-  if not token_payload.access_token or not token_payload.refresh_token or not token_payload.token_type:
+  if not token_payload["access_token"] or not token_payload["refresh_token"] or not token_payload["token_type"]:
     print("API did not return complete token data")
     return
 
-  token_file = Path.home() / settings.TOKEN_STORAGE_PATH
   token_file.write_text(json.dumps(token_payload))
+
+def _load_tokens() -> dict | None:
+  if not token_file.exists():
+    return
+
+  try:
+    return json.loads(token_file.read_text())
+
+  except json.JSONDecodeError:
+    return
