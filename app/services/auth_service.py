c6ad3b5a189b2bd7d18d76eaadca0dfa6fefb374
Eeernest <ernestgrabski16@wp.pl>
2026-09-25 16:04:40 +0200

    feat(auth): add restore_tokens method to AuthService class

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -1,4 +1,4 @@
-from jwt.exceptions import PyJWTError
+from jwt.exceptions import PyJWTError, ExpiredSignatureError
 from redis.exceptions import RedisError
 
 from app.core.config import settings
@@ -7,7 +7,7 @@ from app.core.security import Security
 from app.models.user_model import User
 from app.repositories.token_repository import TokenRepository
 from app.repositories.user_repository import UserRepository
-from app.schemas.token_schema import TokenLogoutRequest, TokenResponse
+from app.schemas.token_schema import TokenLogoutRequest, TokenRestoreRequest, TokenResponse
 
 class AuthService:
   def __init__(
@@ -54,6 +54,33 @@ class AuthService:
     except PyJWTError:
       pass
 
+  async def restore_tokens(self, restore_request: TokenRestoreRequest) -> TokenResponse:
+    decoded_refresh_token = self._try_decode_jwt_token(restore_request.refresh_token)
+
+    self._validate_refresh_token(decoded_refresh_token["refresh"])
+
+    await self._try_delete_refresh_token(decoded_refresh_token["jti"])
+
+    user_obj = await self._try_get_user_by_id(int(decoded_refresh_token["sub"]))
+
+    access_token_payload = self._get_access_token_payload(user_obj)
+    refresh_token_payload = self._get_refresh_token_payload(user_obj)
+
+    await self._try_store_refresh_token(
+      refresh_token_payload["jti"],
+      refresh_token_payload["exp"],
+      refresh_token_payload["sub"]
+    )
+
+    encoded_access_token = self.security.encode_jwt_token(access_token_payload)
+    encoded_refresh_token = self.security.encode_jwt_token(refresh_token_payload)
+
+    return TokenResponse(
+      access_token=encoded_access_token,
+      refresh_token=encoded_refresh_token,
+      token_type="bearer"
+    )
+
 
   # Helper Methods
 
@@ -75,6 +102,16 @@ class AuthService:
 
     return self.security.create_refresh_token_payload(user_data)
 
+  def _try_decode_jwt_token(self, encoded_token: str) -> dict:
+    try:
+      return self.security.decode_jwt_token(encoded_token)
+
+    except ExpiredSignatureError:
+      raise e.TokenExpiredError()
+
+    except PyJWTError:
+      raise e.InvalidCredentialsError()
+
 
   # token_repo
 
@@ -84,6 +121,24 @@ class AuthService:
 
     except RedisError:
       raise e.RedisFailureError()
+
+  async def _try_delete_refresh_token(self, jti: str) -> None:
+    try:
+      await self.token_repo.delete_refresh_token(jti)
+
+    except RedisError:
+      raise e.RedisFailureError()
+
+
+  # user_repo
+
+  async def _try_get_user_by_id(self, id: int) -> User:
+    user_obj = await self.user_repo.get_by_id(id)
+    
+    if not user_obj:
+      raise e.InvalidTokenError()
+
+    return user_obj
 
 
   # Mixed Dependencies
@@ -115,3 +170,10 @@ class AuthService:
 
     if user_obj.is_deleted:
       raise e.UserDeletedError()
+
+
+  # Token data
+
+  def _validate_refresh_token(self, refresh: bool) -> None:
+    if not refresh:
+      raise e.InvalidTokenError()
