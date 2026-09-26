2abd3fe2dbd54cf5a766f9eea58fb08730dd98c4
Eeernest <ernestgrabski16@wp.pl>
2026-09-26 13:22:14 +0200

    chore(conftest): delete token_fixture.py file from pytest_plugins

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,7 +14,6 @@ from app.schemas.user_schema import UserRole
 pytest_plugins = [
   "anyio",
   "tests.fixtures.auth_fixture",
-  "tests.fixtures.token_fixture",
   "tests.fixtures.user_fixture",
 ]
 
