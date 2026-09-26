1bdbf67e638fe2d8bd4e1dc9d4e673c37caeadd9
Eeernest <ernestgrabski16@wp.pl>
2026-09-26 13:20:08 +0200

    refactor(auth): replace request and response token schemas with auth schemas in auth_router.py functions

--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -2,11 +2,11 @@ from fastapi import APIRouter, Depends, status
 from fastapi.security import OAuth2PasswordRequestForm
 
 from app.dependencies.auth_dependency import AuthServiceDep
-from app.schemas.token_schema import TokenLogoutRequest, TokenRestoreRequest, TokenResponse
+import app.schemas.auth_schema as s
 
 router = APIRouter()
 
-@router.post("/login", response_model=TokenResponse, status_code=status.HTTP_200_OK)
+@router.post("/login", response_model=s.LoginResponse, status_code=status.HTTP_200_OK)
 async def login(
   service: AuthServiceDep,
   login_data: OAuth2PasswordRequestForm = Depends()
@@ -14,15 +14,18 @@ async def login(
   return await service.login(login_data.username, login_data.password)
 
 @router.post("/logout", status_code=status.HTTP_204_NO_CONTENT)
-async def logout(service: AuthServiceDep, logout_request: TokenLogoutRequest):
-  await service.logout(logout_request)
+async def logout(service: AuthServiceDep, logout_request: s.LogoutRequest):
+  await service.logout(logout_request.refresh_token)
 
   return status.HTTP_204_NO_CONTENT
 
 @router.post(
   "/restore_tokens",
-  response_model=TokenResponse,
+  response_model=s.RestoreTokensResponse,
   status_code=status.HTTP_200_OK
 )
-async def restore_tokens(service: AuthServiceDep, restore_request: TokenRestoreRequest):
-  return await service.restore_tokens(restore_request)
+async def restore_tokens(
+  service: AuthServiceDep,
+  restore_request: s.RestoreTokensRequest
+):
+  return await service.restore_tokens(restore_request.refresh_token)
